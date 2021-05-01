c222ba317cfbbe3f11ce0990f8a1774578beca8b
Debian <debian@debian10.linuxvmimages.local>
2021-05-01 04:37:27 -0500

    change

--- main.py
+++ main.py
@@ -16,46 +16,20 @@ def getNumericInput(displayString):
             print("please insert a number")
 
 def addCar():
-    car = {
-            "template" : {
-                "car_types" : [
-                    "sedan",
-                    "SUV",
-                    "hatchback",
-                    "racing",
-                    "coupe",
-                    "minivan",
-                    "pickup"
-                ],
-                "year_of_production" : 0,
-                "year_of_purchase" : 0,
-                "mark" : "",
-                "model" : "",
-                "volume_of_engine" : 0,
-                "power_source" : [
-                    "fuel",
-                    "electricity",
-                    "diesel",
-                    "gas"
-                ],
-                "tank" : 0,
-                "fuel_consumption" : 0,
-                "metric" : [
-                    "mile",
-                    "km"
-                    ]
-                } ,
-            }
-    car["car_types"] = input("Please insert the car type: ")
-    car["year_of_production"] = getNumericInput("Please insert the year of production:")
-    car["year_of_purchase"] = getNumericInput("Please insert the year of purchase:")
-    car["mark"] = input("Please insert the mark of the car:")
-    car["model"] = input("Please insert the model of the car:")
-    car["volume_of_engine"] = getNumericInput("Please insert the engine volume of the car:")
-    car["power_source"] = input("please insert the power source: fuel, gas, diesel or electricity:")
-    car["tank"] = getNumericInput("please insert the volume of the tank in liters:")
-    car["fuel_consumption"] = getNumericInput("please insert the fuel-consumption per 100km:")
-    car["metric"] = input("km or miles:")
+    
+    with open('cardash.json') as file_data:
+        print(file_data)
+        car = json.load(file_data)
+        car["car_types"] = input("Please insert the car type: ")
+        car["year_of_production"] = getNumericInput("Please insert the year of production:")
+        car["year_of_purchase"] = getNumericInput("Please insert the year of purchase:")
+        car["mark"] = input("Please insert the mark of the car:")
+        car["model"] = input("Please insert the model of the car:")
+        car["volume_of_engine"] = getNumericInput("Please insert the engine volume of the car:")
+        car["power_source"] = input("please insert the power source: fuel, gas, diesel or electricity:")
+        car["tank"] = getNumericInput("please insert the volume of the tank in liters:")
+        car["fuel_consumption"] = getNumericInput("please insert the fuel-consumption per 100km:")
+        car["metric"] = input("km or miles:")
     return car
 
 def loadExistingCars():
@@ -89,7 +63,6 @@ def main():
     displayAllCars(cars)
 
 main()
-
 
 
 
